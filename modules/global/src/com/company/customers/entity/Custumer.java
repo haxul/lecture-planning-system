75c65cf5593ac5c55b2fbe5b1b23f51155cecda7
haxul <s.s.starodubov@yandex.ru>
2020-01-25 18:46:26 +0400

    init

---
+++ modules/global/src/com/company/customers/entity/Custumer.java
@@ -0,0 +1,12 @@
+package com.company.customers.entity;
+
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Table(name = "CUSTOMERS_CUSTUMER")
+@Entity(name = "customers_Custumer")
+public class Custumer extends StandardEntity {
+    private static final long serialVersionUID = -6465505201356924838L;
+}
